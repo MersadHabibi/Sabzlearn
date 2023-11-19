8137d91e678d41ed73fda171bd70692b4036170f
mersad <mersad.up@gmail.com>
2023-11-19 17:48:10 +0330

    Refactor And Debug All Codes

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -2,109 +2,80 @@ import { _changeClasses } from "./funcs/utils.js";
 
 const $ = document;
 
-let isSearchOpen = false;
-let isProfileMenuOpen = false;
-let isSubmenuOpen = false;
-let isMobileMenuOpen = false;
+let openElem = {};
 
 const searchBtn = $.querySelector(".search-btn");
-const searchBox = $.querySelector(".search-box");
-const cover = $.querySelector(".cover");
-const profileMenuBtn = $.querySelector(".profile-menu-btn");
-const profileMenu = $.querySelector(".profile-menu");
+const overlay = $.querySelector(".overlay");
+const profile = $.querySelector(".profile");
 const mobileSubmenuBtns = $.querySelectorAll(".mobile__submenu-btn");
-const mobileMenu = $.querySelector(".mobile__menu");
+const mobileMenu = $.querySelector(".mobile-menu");
 const mobileMenuCloseBtn = $.querySelector(".mobile__close-btn");
 const mobileMenuOpenBtn = $.querySelector(".mobile-menu__open-btn");
-const mobileMenuCover = $.querySelector(".mobile-menu__cover");
 
-const openCover = () => {
-  _changeClasses("remove", cover, ["opacity-0", "-z-10"]);
-  _changeClasses("add", cover, ["opacity-1", "z-10"]);
-};
-const closeCover = () => {
-  _changeClasses("add", cover, ["opacity-0", "-z-10"]);
-  _changeClasses("remove", cover, ["opacity-1", "z-10"]);
+const openCloseoverlay = () => {
+  _changeClasses("toggle", overlay, ["show"]);
 };
 
-const openSearchBox = () => {
-  openCover();
-  _changeClasses("remove", searchBox, ["hidden"]);
-  _changeClasses("add", searchBtn, ["z-20"]);
-  isSearchOpen = true;
-};
-const closeSearchBox = () => {
-  closeCover();
-  _changeClasses("add", searchBox, ["hidden"]);
-  _changeClasses("remove", searchBtn, ["z-20"]);
-  isSearchOpen = false;
+const openCloseSearchBox = () => {
+  _changeClasses("toggle", searchBtn, ["search-btn--active"]);
+  openCloseoverlay();
+
+  openElem = openElem == searchBtn ? null : searchBtn;
 };
 
-const openProfileMenu = () => {
-  openCover();
-  _changeClasses("remove", profileMenu, ["hidden"]);
-  _changeClasses("add", profileMenuBtn, ["z-20"]);
-  isProfileMenuOpen = true;
-};
-const closeProfileMenu = () => {
-  closeCover();
-  _changeClasses("add", profileMenu, ["hidden"]);
-  _changeClasses("remove", profileMenuBtn, ["z-20"]);
-  isProfileMenuOpen = false;
+const openCloseProfileMenu = () => {
+  _changeClasses("toggle", profile, ["profile--active"]);
+  openCloseoverlay();
+
+  openElem = openElem == profile ? null : profile;
 };
 
 const openAndCloseMobileMenuSubmenu = (e) => {
   let clickedElem = e.target.parentElement.parentElement;
-  if (isSubmenuOpen) {
-    if (clickedElem.classList.contains("submenu--active")) {
-      _changeClasses("remove", clickedElem, ["submenu--active"]);
-      isSubmenuOpen = false;
-    } else {
-      _changeClasses("remove", $.querySelector(".submenu--active"), ["submenu--active"]);
-      _changeClasses("add", clickedElem, ["submenu--active"]);
-    }
+  if (clickedElem.classList.contains("menu__item--active")) {
+    _changeClasses("remove", clickedElem, ["menu__item--active"]);
   } else {
-    _changeClasses("add", clickedElem, ["submenu--active"]);
-    isSubmenuOpen = true;
+    const activeElem = $.querySelector(".menu__item--active");
+    _changeClasses("remove", activeElem, ["menu__item--active"]);
+    _changeClasses("add", clickedElem, ["menu__item--active"]);
   }
 };
 
-const closeMobileMenu = () => {
-  _changeClasses("remove", mobileMenu, ["mobile__menu--open"]);
-  closeCover();
+const openCloseMobileMenu = (action) => {
+  if (action == "open") {
+    _changeClasses("add", mobileMenu, ["mobile-menu--open"]);
+    openCloseoverlay();
+
+    openElem = mobileMenu;
+  } else {
+    _changeClasses("remove", mobileMenu, ["mobile-menu--open"]);
+    openCloseoverlay();
+
+    openElem = null;
+  }
 };
 
-const openMobileMenu = () => {
-  _changeClasses("add", mobileMenu, ["mobile__menu--open"]);
-  openCover();
-};
+searchBtn.addEventListener("click", openCloseSearchBox);
 
-searchBtn.addEventListener("click", () => {
-  if (isSearchOpen) {
-    closeSearchBox();
+profile.addEventListener("click", openCloseProfileMenu);
+
+overlay.addEventListener("click", () => {
+  if (openElem == searchBtn) {
+    openCloseSearchBox();
+  } else if (openElem == profile) {
+    openCloseProfileMenu();
   } else {
-    openSearchBox();
+    openCloseMobileMenu("close");
   }
-});
-
-profileMenuBtn.addEventListener("click", () => {
-  if (isProfileMenuOpen) {
-    closeProfileMenu();
-  } else {
-    openProfileMenu();
-  }
-});
-
-cover.addEventListener("click", () => {
-  closeCover();
-  closeProfileMenu();
-  closeSearchBox();
-  closeMobileMenu();
 });
 
 mobileSubmenuBtns.forEach((btn) => {
   btn.addEventListener("click", openAndCloseMobileMenuSubmenu);
 });
 
-mobileMenuCloseBtn.addEventListener("click", closeMobileMenu);
-mobileMenuOpenBtn.addEventListener("click", openMobileMenu);
+mobileMenuCloseBtn.addEventListener("click", () => {
+  openCloseMobileMenu("close");
+});
+mobileMenuOpenBtn.addEventListener("click", () => {
+  openCloseMobileMenu("open");
+});
