6985a2a9f10f167c2ba57d60d23a916a54a01448
mersad <mersad.up@gmail.com>
2023-11-23 13:21:42 +0330

    Create: presell courses section

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,8 +1,12 @@
 const $ = document;
+// New Courses
 const newCoursesSliderPrevBtn = $.querySelector(".new-courses-slider-prev");
 const newCoursesSliderNextBtn = $.querySelector(".new-courses-slider-next");
+// Presell Courses
+const presellCoursesSliderPrevBtn = $.querySelector(".presell-courses-slider-prev");
+const presellCoursesSliderNextBtn = $.querySelector(".presell-courses-slider-next");
 
-const newCoursesSlider = new Swiper(".mySwiper", {
+const slidersConfig = {
   slidesPerView: 1,
   loop: true,
   autoplay: {
@@ -23,8 +27,22 @@ const newCoursesSlider = new Swiper(".mySwiper", {
       spaceBetween: 20,
     },
   },
-});
+};
+
+// New Course Slider
+const newCoursesSlider = new Swiper(".new-courses .mySwiper", slidersConfig);
 newCoursesSliderNextBtn.addEventListener("click", () => {
   newCoursesSlider.slideNext();
 });
-newCoursesSliderPrevBtn.addEventListener("click", newCoursesSlider.slidePrev);
+newCoursesSliderPrevBtn.addEventListener("click", () => {
+  newCoursesSlider.slidePrev();
+});
+
+// Presell Courses Slider
+const presellCoursesSlider = new Swiper(".presell-courses .mySwiper", slidersConfig);
+presellCoursesSliderNextBtn.addEventListener("click", () => {
+  presellCoursesSlider.slideNext();
+});
+presellCoursesSliderPrevBtn.addEventListener("click", () => {
+  presellCoursesSlider.slidePrev();
+});
