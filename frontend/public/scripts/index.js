5e55e556aba41cc7d6cf728f688d6a63e893681a
mersad <mersad.up@gmail.com>
2023-12-19 22:47:33 +0330

    coding get and show new courses

--- frontend/public/scripts/index.js
+++ frontend/public/scripts/index.js
@@ -1,10 +1,23 @@
+import { createCourseCard } from "./funcs/share.js";
+import { api } from "./funcs/utils.js";
+
 const $ = document;
+
 // New Courses
+
 const newCoursesSliderPrevBtn = $.querySelector(".new-courses-slider-prev");
 const newCoursesSliderNextBtn = $.querySelector(".new-courses-slider-next");
+
 // Presell Courses
-const presellCoursesSliderPrevBtn = $.querySelector(".presell-courses-slider-prev");
-const presellCoursesSliderNextBtn = $.querySelector(".presell-courses-slider-next");
+
+const presellCoursesSliderPrevBtn = $.querySelector(
+  ".presell-courses-slider-prev"
+);
+const presellCoursesSliderNextBtn = $.querySelector(
+  ".presell-courses-slider-next"
+);
+
+// Slider Config
 
 const slidersConfig = {
   slidesPerView: 1,
@@ -30,19 +43,65 @@ const slidersConfig = {
 };
 
 // New Course Slider
+
 const newCoursesSlider = new Swiper(".new-courses .mySwiper", slidersConfig);
+
 newCoursesSliderNextBtn.addEventListener("click", () => {
   newCoursesSlider.slideNext();
 });
+
 newCoursesSliderPrevBtn.addEventListener("click", () => {
   newCoursesSlider.slidePrev();
 });
 
 // Presell Courses Slider
-const presellCoursesSlider = new Swiper(".presell-courses .mySwiper", slidersConfig);
+
+const presellCoursesSlider = new Swiper(
+  ".presell-courses .mySwiper",
+  slidersConfig
+);
+
 presellCoursesSliderNextBtn.addEventListener("click", () => {
   presellCoursesSlider.slideNext();
 });
+
 presellCoursesSliderPrevBtn.addEventListener("click", () => {
   presellCoursesSlider.slidePrev();
 });
+
+// Get Course
+
+const res = await fetch(`${api}admin/courses`);
+let courses = await res.json();
+
+// Get And Show Last Courses - random
+
+const getAndShowLastCourses = async () => {
+  const lastCoursesContainer = $.querySelector(".last-courses__container");
+
+  courses = courses.sort(() => Math.random() - 0.5);
+
+  courses.slice(0, 8).forEach((course) => {
+    lastCoursesContainer.insertAdjacentHTML(
+      "beforeend",
+      createCourseCard(course, "last-courses")
+    );
+  });
+};
+getAndShowLastCourses();
+
+// Get And Show New Courses
+
+const getAndShowNewCourses = async () => {
+  const newCoursesContainer = $.querySelector(".new-courses__container");
+
+  courses.slice(0, 8).forEach((course) => {
+    newCoursesContainer.insertAdjacentHTML(
+      "beforeend",
+      `
+          ${createCourseCard(course, "new-courses")}
+      `
+    );
+  });
+};
+getAndShowNewCourses();
