fa3e879499cc937cd0588f721c04894b3da46355
mrnobody1384 <ostoore69@gmail.com>
2024-01-11 21:52:58 +0330

    Comment Schema Edited & DeleteCourse Tested

--- backend/controller/Admin/deleteCourse.js
+++ backend/controller/Admin/deleteCourse.js
@@ -1,20 +1,36 @@
-import { PrismaClient } from "@prisma/client"
-const prisma = new PrismaClient()
-function deleteCourse(req,res) {
-    prisma.course
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+import Joi from "joi";
+const schema = Joi.object({
+  id: Joi.string().required().min(1).trim(),
+});
+function deleteCourse(req, res) {
+  schema
+    .validateAsync({ id: req.params.id })
+    .then((reqId) => {
+      prisma.course
         .delete({
-            where: {
-                id: req.params.id
-            }
+          where: {
+            id: reqId.id,
+          },
         })
         .then((course) => {
-            res.json({ message: "Your Course Deleted Successfully." })
+          res.json({ message: "Your Course Deleted Successfully." });
         })
         .catch((err) => {
-            res
-                .status(500)
-                .json({ message: "there is an error in deleting course", err })
-        })
+          res
+            .status(500)
+            .json({ message: "there is an error in deleting course", err });
+        });
+    })
+    .catch((err) => {
+      res
+        .status(403)
+        .json({
+          message: "there is an error in your sended Data.",
+          err: err?.details[0]?.message,
+        });
+    });
 }
 
-export default deleteCourse
+export default deleteCourse;
