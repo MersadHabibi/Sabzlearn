8b405a65666e86b84b14f1fe067693cb9e4918d5
mersad <mersad.up@gmail.com>
2024-02-19 19:29:12 +0330

    Edit : submit_otp_page

--- frontend/src/scripts/submit_otp_page.js
+++ frontend/src/scripts/submit_otp_page.js
@@ -1,1 +1,11 @@
 import "../styles/app.css";
+
+const otpInputs = document.querySelectorAll(".otp__input");
+
+otpInputs.forEach(input => {
+  input.addEventListener("input", () => {
+    const nextInput = input.previousElementSibling;
+
+    if (input.value.length === 1 && nextInput) nextInput.focus();
+  });
+});
