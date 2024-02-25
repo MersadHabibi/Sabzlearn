42541cd4eee5d483619e276e6aa4a98fc24df305
mrnobody1384 <ostoore69@gmail.com>
2024-02-25 16:27:28 +0330

    Added VerifyOtp Route.

--- backend/api/routes.js
+++ backend/api/routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import register from "../Authentication/register.js";
+import register, { verifyOtp } from "../Authentication/register.js";
 import login from "../Authentication/login.js";
 import AuthCheckMiddleWare from "../Authentication/Middleware/index.js";
 import getMe from "../controller/getMe.js";
@@ -14,6 +14,7 @@ import getAllCategories from "../controller/Admin/getAllCategories.js";
 const router = Router();
 
 router.post("/register", register);
+router.post("/verify-otp", verifyOtp);
 router.post("/login", login);
 router
   .route("/me")
